9fb9babf8382cc32a90ce0b37aedd83df9bef6dd
Anuj aggarwal <anujaggarwal67@gmail.com>
2021-01-19 14:16:27 +0530

    'first'

---
+++ realtors/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from realtors.models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date')
+    list_display_links = ('id','name')
+    search_fields = ('name','email')
+    list_per_page = 25
+
+# Register your models here.
+admin.site.register(Realtor,RealtorAdmin)
